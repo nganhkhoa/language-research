b92a4a404da99250b4eb95c0b2ef5278f61947cc
nganhkhoa <service.nganhkhoa@gmail.com>
2025-11-01 21:22:52 -0500

    [rize] update interp

--- contracts/interp.rkt
+++ contracts/interp.rkt
@@ -1,5 +1,7 @@
 #lang plai
 
+(require racket/trace)
+
 (require "ast.rkt")
 
 (define-type rz-value
@@ -8,83 +10,138 @@
              [rz-bool (b boolean?)]
              [rz-list (hd rz-value?) (rst rz-value?)]
              [rz-nil]
-             [rz-function (id symbol?) (body rize-expr?)]
+             [rz-function (id symbol?) (body rz-expr?)]
+             [rz-app (funclike rz-value?) (arg rz-value?)]
              [rz-fcontract (c rz-value?)]
              [rz-hcontract (dom rz-value?)
                            (rng rz-value?)]
              [rz-obligation (v rz-value?)
-                            (c rize-expr?)
+                            (c rz-value?)
                             (in symbol?)
                             (out symbol?)])
 
-(define (run (program (listof rize-expr?)))
+(define (run (program (listof rz-expr?)) (decls (listof rz-decl?)))
   ;; we actually don't want multiple expression, but just ignore it
   ;; because transform gives us an "inverted" program so we fold right
   (foldr (lambda (expr _)
            (with-handlers
              ([exn:fail?
                (lambda (exn) (printf "error ~a\n" exn))])
-             (printf "~a\n" (interp expr))))
+             (printf "~a\n" (interp expr decls))))
          '() program))
 
-(define (interp (expr rize-expr?))
-  ;; (printf "interp ~a\n" expr)
-  (type-case rize-expr expr
+(define (get-name (name symbol?) (decls (listof rz-decl?)))
+  (val-expr (car (filter (lambda (decl) (eq? name (val-id decl))) decls))))
+
+(define (interp (expr rz-expr?) (decls (listof rz-decl?)))
+  (type-case rz-expr expr
              ;; some first order element
+             [id (name) (interp (get-name name decls) decls)]
              [num (n) (rz-num n)]
              [str (s) (rz-str s)]
              [bool (b) (rz-bool b)]
              [nil () (rz-nil)]
              [condition (e t-branch f-branch)
-                        (type-case rz-value (interp e)
+                        (type-case rz-value (interp e decls)
                           [rz-bool (b) (cond
-                                         [b (interp t-branch)]
-                                         [else (interp f-branch)])]
-                          [rz-num (n) (cond
-                                        [(eq? n 0) (interp f-branch)]
-                                        [else (interp t-branch)])]
+                                         [b (interp t-branch decls)]
+                                         [else (interp f-branch decls)])]
                           ;; everything else is treated as true
                           ;; whatever, type check should check this
-                          [else (interp t-branch)])]
-             [aop (op lhs rhs) (perform-op op (interp lhs) (interp rhs))]
-             [rop (op lhs rhs) (perform-op op (interp lhs) (interp rhs))]
+                          [else (error 'interp "if case can only switch on boolean")])]
+             [aop (op lhs rhs) (perform-op op (interp lhs decls) (interp rhs decls))]
+             [rop (op lhs rhs) (perform-op op (interp lhs decls) (interp rhs decls))]
              [concat (left right)
-                     (rz-list (interp left) (interp right))]
+                     (rz-list (interp left decls) (interp right decls))]
 
              [head (lst)
-                   (type-case rz-value (interp lst)
+                   (type-case rz-value (interp lst decls)
                      [rz-list (hd _) hd]
                      [rz-nil ()
                              (error 'interp "cannot get head of nil ~a" expr)]
                      [else (error 'interp "cannot get head of non-list ~a" expr)])]
              [tail (lst)
-                   (type-case rz-value (interp lst)
+                   (type-case rz-value (interp lst decls)
                      [rz-list (_ rst) rst]
                      [rz-nil ()
                              (error 'interp "cannot get head of nil ~a" expr)]
                      [else (error 'interp "cannot get head of non-list ~a" expr)])]
              [mt (lst)
-                  (type-case rz-value (interp lst)
+                  (type-case rz-value (interp lst decls)
                     [rz-list (a b) (rz-bool false)]
                     [rz-nil () (rz-bool true)]
                     [else (error 'interp "non-list cannot check for empty ~a" expr)])]
 
-             ;; function
              [function (arg body)
                        (rz-function arg body)]
-             ;; TODO: fix fixpoint function
-             [fixpoint (arg body)
-                       (rz-function arg body)]
+             [fixpoint (param body)
+                       (interp (subst param (fixpoint param body) body) decls)]
 
-             [application (funclike arg) (interp-apply (interp funclike) arg)]
-             ;; [h-contract (dom rng)
-             ;;             (rz-hcontract (interp dom) (interp rng))]
-             ;; [f-contract (c) (rz-fcontract (interp c))]
+             [application (funclike arg)
+                          (define f (interp funclike decls))
+                          (define a (interp arg decls))
+                          (printf "apply ~a ~a\n" f a)
+                          (type-case rz-value f
+                                     [rz-function (param body) (interp (subst param a body) decls)]
+                                     [rz-obligation (v c in out)
+                                                    ;; make new obligation value yay
+                                                    (unless (rz-hcontract? c)
+                                                      (error 'interp "expecting a higher order contract to apply"))
+                                                    (define arg-guard (obligation-reduce (rz-obligation a (rz-hcontract-dom c) out in) decls))
+                                                    (obligation-reduce (rz-obligation (rz-app v arg-guard) (rz-hcontract-rng c) in out) decls)]
+                                     [else (error 'interp "cannot call a non-callable value ~a" f)])
+                          ]
+             [h-contract (dom rng) (rz-hcontract (interp dom decls) (interp rng decls))]
+             [f-contract (c) (rz-fcontract (interp c decls))]
              [obligation (e c in out)
-                         (rz-obligation (interp e) c in out)]
+                         (define c-value (interp c decls))
+                         (define e-value (interp e decls))
+                         (obligation-reduce (rz-obligation e-value c-value in out) decls)]
              [else (error 'interp "unimplemented ~a" expr)]))
+(trace interp)
 
-(define (interp-apply (funclike rz-value?) (arg rize-expr?))
+(define (obligation-reduce (obligation rz-value?) (decls (listof rz-decl?)))
+  ;; higher order contract
+  ;; example includes: V1^{A1->A2}^{B1->B2} V2
+  ;; reduces to (V1^{A1->A2} V2^{B1})^{B2}
+  ;; and further reduces to (V1 V2^{B1}^{A1})^{A2}^{B2}
+  ;; this is by placing multiple constraints after going
+  ;; through multiple function calls
+
+  ;; two main rules
+  ;; v^{contract(c)} -> if c(v) then v else blame
+  ;; v1^{A->B} v2 -> (v1 v2^{A})^{B}
+  ;; idk which rule should be applied first
+  ;; but for the sake of simplicity, we check flat contract first then application
+  (type-case rz-value obligation
+             [rz-obligation (v c in out)
+                            ;; if e is rz-app then move on and apply the function
+                            (define reduced-v (type-case rz-value v
+                                          [rz-app (function argument)
+                                                  (type-case rz-value function
+                                                             [rz-function (param body)
+                                                                          (interp (subst param argument body) decls)]
+                                                             [else (error 'interp "higher order contract works with function only not ~a" function)])]
+                                          [else v]))
+                            (cond
+                              [(rz-hcontract? c)
+                               ;; high order contract, e must be a call-able or rz-app
+                               (rz-obligation reduced-v c in out)]
+                              [(rz-fcontract? c)
+                               ;; flat contract, e must be a basic value
+                               (type-case rz-value (rz-fcontract-c c)
+                                          ;; check contract
+                                          [rz-function (param body)
+                                                       (define condition (interp (subst param reduced-v body) decls))
+                                                       (type-case rz-value condition
+                                                                  [rz-bool (b) (if b reduced-v (error 'interp "rize contract violation contract:~a value:~a at:~a" c reduced-v in))]
+                                                                  [else (error 'interp "contract must return a boolean value")])]
+                                          [else (error 'interp "contract is not callable ~a" c)])])]
+             [else (error 'interp "can only reduce an obligation not ~a" obligation)]))
+
+(trace obligation-reduce)
+
+(define (interp-apply (funclike rz-value?) (arg rz-expr?))
   ;; (printf "apply function ~a ~a\n" funclike arg)
   (type-case rz-value funclike
     ;; this feels awkward, because body is rz-value
@@ -149,10 +206,15 @@
              [rz-function (param body)
                           (function param body)]
              [rz-nil () (nil)]
+             [rz-obligation (v c in out)
+                            (obligation (as-expr v) (as-expr c) in out)]
+             [rz-hcontract (dom rng) (h-contract (as-expr dom) (as-expr rng))]
+             [rz-fcontract (c) (f-contract (as-expr c))]
              [else error 'interp "can only convert basic value to expr ~a" v]))
 
-(define (subst arg value expr)
-  (type-case rize-expr expr
+(define (subst arg maybe-value expr)
+  (define value (if (rz-expr? maybe-value) maybe-value (as-expr maybe-value)))
+  (type-case rz-expr expr
              [id (name) (if (eq? name arg)
                             value
                             (id name))]
@@ -182,13 +244,14 @@
                                        (subst arg value body))]
              ;; will error when interp if not correct
              [else expr]))
+(trace subst)
 
 
-(define (contract-guard (value rize-expr?) (is-in boolean?))
+(define (contract-guard (value rz-expr?) (is-in boolean?))
   (define error-msg (lambda (v c in out)
                       (error 'interp "contract violation ~a of ~a by ~a"
                              c v (if is-in in out))))
-  (type-case rize-expr value
+  (type-case rz-expr value
     ;; this can be h-contract also
     ;; the reason why is when you call a function with a function
     ;; f: (a -> b) -> c
@@ -214,6 +277,7 @@
       ['/ (rz-num (/ left right))]
       ['>= (rz-bool (>= left right))]
       ['= (rz-bool (= left right))]
+      [_ (error 'interp "cannot perform operation ~a" op)]
       ;; no extra case, guard by parser
       ))
   (cond
